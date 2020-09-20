4068dfcf19a51163e282a39f8316a26b8c7a4410
William Roger <williamroger.frontend@gmail.com>
2020-09-20 00:59:56 -0300

    Criado as primeiras Types e Mutations no GraphQL

--- src/index.js
+++ src/index.js
@@ -1,15 +1,75 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
   type Query {
-    hello: String,
+    hello: String
+    users: [User!]!
+    getUserByEmail(email: String!): User!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
   }
 `;
 
+const users = [
+  {
+    _id: String(Math.random()),
+    name: "William",
+    email: "william@test.com",
+    active: true,
+  },
+  {
+    _id: String(Math.random()),
+    name: "William 2",
+    email: "william2@test.com",
+    active: false,
+  },
+  {
+    _id: String(Math.random()),
+    name: "William 3",
+    email: "william3@test.com",
+    active: true,
+  },
+];
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World',
-  }
+    hello: () => "Hello World",
+    users: () => users,
+    getUserByEmail: (_, args) => {
+      return users.find((user) => user.email === args.email);
+    },
+  },
+
+  Mutation: {
+    createUser: (_, args) => {
+      const newUser = {
+        _id: String(Math.random()),
+        name: args.name,
+        email: args.email,
+        active: true,
+      }
+
+      users.push(newUser);
+
+      return newUser;
+    }
+  },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
